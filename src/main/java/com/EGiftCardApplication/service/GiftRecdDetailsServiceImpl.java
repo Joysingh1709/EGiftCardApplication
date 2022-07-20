337e24ce51b6c111cda783d6bc947b286e3764de
Joy Singh <avnishmamta060606@gmail.com>
2022-07-20 14:17:23 +0530

    gift rec details updated

--- src/main/java/com/EGiftCardApplication/service/GiftRecdDetailsServiceImpl.java
+++ src/main/java/com/EGiftCardApplication/service/GiftRecdDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.EGiftCardApplication.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -24,6 +25,24 @@ public class GiftRecdDetailsServiceImpl implements GiftRecdDetailsService {
 
 	@Override
 	public Gift_recd_details addGift_recd_details(Gift_recd_details giftRecdDetails) {
+		Gift_recd_details grd = giftRecdRepo.save(giftRecdDetails);
+		if (grd != null) {
+			return grd;
+		}
+		return null;
+	}
+
+	@Override
+	public Gift_recd_details getGift_recd_detailsById(Long serialNumber) {
+		Optional<Gift_recd_details> gift = giftRecdRepo.findById(serialNumber);
+		if (gift.isPresent()) {
+			return gift.get();
+		}
+		return null;
+	}
+
+	@Override
+	public Gift_recd_details updateGift_recd_details(Gift_recd_details giftRecdDetails) {
 		Gift_recd_details grd = giftRecdRepo.save(giftRecdDetails);
 		if (grd != null) {
 			return grd;
