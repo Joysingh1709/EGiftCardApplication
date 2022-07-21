0ffb21bd3643c1bedf00a914f8293966f4abe764
Joy Singh <avnishmamta060606@gmail.com>
2022-07-21 18:35:51 +0530

    lets deploy

--- src/main/java/com/EGiftCardApplication/EGiftCardApplication.java
+++ src/main/java/com/EGiftCardApplication/EGiftCardApplication.java
@@ -3,10 +3,8 @@ package com.EGiftCardApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class EGiftCardApplication {
 
 	public static void main(String[] args) {
